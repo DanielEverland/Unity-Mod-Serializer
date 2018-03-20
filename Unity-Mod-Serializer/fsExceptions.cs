eade065044b570eff6132636b2094b99e1581b60
Daniel Everland <danieleverland@gmail.com>
2018-03-20 18:42:18 +0100

    Added FullSerializer

---
+++ Unity-Mod-Serializer/fsExceptions.cs
@@ -0,0 +1,17 @@
+﻿// note: This file contains exceptions used by UMS. Exceptions are
+//       never used at runtime in UMS; they are only used when
+//       validating annotations and code-based models.
+
+using System;
+
+namespace UMS {
+    public sealed class fsMissingVersionConstructorException : Exception {
+        public fsMissingVersionConstructorException(Type versionedType, Type constructorType) :
+            base(versionedType + " is missing a constructor for previous model type " + constructorType) { }
+    }
+
+    public sealed class fsDuplicateVersionNameException : Exception {
+        public fsDuplicateVersionNameException(Type typeA, Type typeB, string version) :
+            base(typeA + " and " + typeB + " have the same version string (" + version + "); please change one of them.") { }
+    }
+}
