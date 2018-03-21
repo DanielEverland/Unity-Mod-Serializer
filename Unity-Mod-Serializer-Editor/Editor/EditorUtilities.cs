db2839f5ecca4e533307ce32610584a4cbcc6270
Daniel Everland <danieleverland@gmail.com>
2018-03-21 13:41:32 +0100

    Added GetGUID function to EditorUtilities

--- Unity-Mod-Serializer-Editor/Editor/EditorUtilities.cs
+++ Unity-Mod-Serializer-Editor/Editor/EditorUtilities.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 using UnityEditor;
 
 namespace UMS.Editor
@@ -12,6 +12,12 @@ namespace UMS.Editor
         public const string MENU_ITEM_ROOT = "Modding";
         public const string MENU_SERIALIZATION = "Serialization";
 
+        public static string GetGUID(Object obj)
+        {
+            string path = AssetDatabase.GetAssetPath(obj);
+
+            return AssetDatabase.AssetPathToGUID(path);
+        }
         public static List<ModPackage> GetAllPackages()
         {
             return new List<ModPackage>(AssetDatabase.FindAssets("t:ModPackage").Select(x =>
