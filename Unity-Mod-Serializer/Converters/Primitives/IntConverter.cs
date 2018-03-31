d0b08200c260f9ab22bfecfcaa877623a8414b45
Daniel Everland <danieleverland@gmail.com>
2018-03-31 23:06:27 +0200

    Added int converter

---
+++ Unity-Mod-Serializer/Converters/Primitives/IntConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMS.Converters.Primitives
+{
+    public sealed class IntConverter : DirectConverter<int>
+    {
+        public override Result TrySerialize(object instance, out Data serialized, Type storageType)
+        {
+            serialized = new Data((int)instance);
+            return Result.Success;
+        }
+        public override Result TryDeserialize(Data data, ref object instance, Type storageType)
+        {
+            if (!data.IsInt64)
+                return Result.Fail("Expected type Int64");
+
+            instance = Convert.ChangeType(data.AsInt64, typeof(int));
+            return Result.Success;
+        }
+    }
+}
---
+++ Unity-Mod-Serializer/Converters/Primitives/UnsignedIntConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMS.Converters.Primitives
+{
+    public sealed class UnsignedIntConverter : DirectConverter<uint>
+    {
+        public override Result TrySerialize(object instance, out Data serialized, Type storageType)
+        {
+            serialized = new Data((uint)instance);
+            return Result.Success;
+        }
+        public override Result TryDeserialize(Data data, ref object instance, Type storageType)
+        {
+            if (!data.IsInt64)
+                return Result.Fail("Expected type Int64");
+
+            instance = Convert.ChangeType(data.AsInt64, typeof(uint));
+            return Result.Success;
+        }
+    }
+}
