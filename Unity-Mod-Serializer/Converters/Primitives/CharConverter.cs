301e88df17dc634eb1674f8b0da9a74b440b42b5
Daniel Everland <danieleverland@gmail.com>
2018-03-31 23:38:43 +0200

    Added char converter

---
+++ Unity-Mod-Serializer/Converters/Primitives/CharConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMS.Converters.Primitives
+{
+    public sealed class CharConverter : DirectConverter<char>
+    {
+        public override Result TrySerialize(object instance, out Data serialized, Type storageType)
+        {
+            serialized = new Data((string)instance);
+            return Result.Success;
+        }
+        public override Result TryDeserialize(Data data, ref object instance, Type storageType)
+        {
+            if (!data.IsString)
+                return Result.Fail("Expected type string");
+
+            instance = data.AsString;
+            return Result.Success;
+        }
+    }
+}
