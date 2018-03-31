47a77ef799e94df500c2a3e8f798767534bd53dd
Daniel Everland <danieleverland@gmail.com>
2018-03-31 23:27:45 +0200

    Implemented decimal converter

---
+++ Unity-Mod-Serializer/Converters/Primitives/DecimalConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMS.Converters.Primitives
+{
+    public sealed class DecimalConverter : DirectConverter<decimal>
+    {
+        public override Result TrySerialize(object instance, out Data serialized, Type storageType)
+        {
+            serialized = new Data((double)Convert.ChangeType(instance, typeof(double)));
+            return Result.Success;
+        }
+        public override Result TryDeserialize(Data data, ref object instance, Type storageType)
+        {
+            if (!data.IsDouble)
+                return Result.Fail("Expected type double");
+
+            instance = Convert.ChangeType(data.AsDouble, typeof(double));
+            return Result.Success;
+        }
+    }
+}
