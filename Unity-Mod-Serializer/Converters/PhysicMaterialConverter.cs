e5ba86fe210cd8e6bffc1da6a2dd9e6c63807ae4
Daniel Everland <danieleverland@gmail.com>
2018-04-01 16:08:04 +0200

    Added support for physic materials

---
+++ Unity-Mod-Serializer/Converters/PhysicMaterialConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UMS.ConverterHelpers;
+
+namespace UMS.Converters
+{
+    public class PhysicMaterialConverter : DirectConverter<PhysicMaterial>
+    {
+        private static readonly List<string> _members = new List<string>()
+        {
+            "dynamicFriction",
+            "staticFriction",
+            "bounciness",
+            "frictionCombine",
+            "bounceCombine",
+        };
+
+        public override object CreateInstance(Data data, Type storageType)
+        {
+            return new PhysicMaterial();
+        }
+        protected override Result DoSerialize(PhysicMaterial model, Dictionary<string, Data> serialized)
+        {
+            Result result = Result.Success;
+
+            result += SerializeMembers(serialized, model, _members);
+            result += UnityEngineObjectHelper.TrySerialize(serialized, model);
+
+            return result;
+        }
+        protected override Result DoDeserialize(Dictionary<string, Data> data, ref PhysicMaterial model)
+        {
+            Result result = Result.Success;
+
+            result += SerializeMembers(data, model, _members);
+            result += UnityEngineObjectHelper.TryDeserialize(data, model);
+
+            return result;
+        }
+    }
+}
---
+++ Unity-Mod-Serializer/EntryWriters/PhysicMaterialEntryWriter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UMS.EntryWriters
+{
+    public class PhysicMaterialEntryWriter : UnityEngineObjectEntryWriter<PhysicMaterial>
+    {
+        protected override string GetExtension(PhysicMaterial obj)
+        {
+            return "physicMaterial";
+        }
+        protected override string GetFileName(PhysicMaterial obj)
+        {
+            return string.Format("{0}/Materials/{1}", StaticObjects.FOLDER_NAME, obj.name);
+        }
+    }
+}
