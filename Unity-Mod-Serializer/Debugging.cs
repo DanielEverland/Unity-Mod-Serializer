976dad54f70d7d25c9d5a011682f7e4de5047ae5
Daniel Everland <danieleverland@gmail.com>
2018-04-04 16:50:13 +0200

    Added debugging class

---
+++ Unity-Mod-Serializer/Debugging.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UMS
+{
+    public static class Debugging
+    {
+        public static void Force(DebuggingLevels level, string message)
+        {
+            switch (level)
+            {
+                case DebuggingLevels.Info:
+                    Debug.Log(message);
+                    break;
+                case DebuggingLevels.Warning:
+                    Debug.LogWarning(message);
+                    break;
+                case DebuggingLevels.Error:
+                    Debug.LogError(message);
+                    break;
+                default:
+                    throw new ArgumentException("Unexpected level");
+            }
+        }
+        public static void Info(DebuggingFlags flags, string message)
+        {
+            Output(flags, DebuggingLevels.Info, message, Debug.Log);
+        }
+        public static void Warning(DebuggingFlags flags, string message)
+        {
+            Output(flags, DebuggingLevels.Warning, message, Debug.LogWarning);
+        }
+        public static void Error(DebuggingFlags flags, string message)
+        {
+            Output(flags, DebuggingLevels.Error, message, Debug.LogError);
+        }
+        private static void Output(DebuggingFlags flags, DebuggingLevels levels, string message, Action<string> action)
+        {
+#if !DEBUG
+            if (!Application.isEditor && !Settings.DebugInBuiltVersion)
+                return;
+#endif
+
+            if ((Settings.DebuggingLevels & levels) != levels)
+                return;
+
+            if ((Settings.DebuggingFlags & flags) != flags)
+                return;
+
+            action(message);
+        }
+    }
+}
