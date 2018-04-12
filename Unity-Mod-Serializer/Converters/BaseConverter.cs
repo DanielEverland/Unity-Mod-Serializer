8954ca857110e7f801846fcb0b8c0626325ff5e1
Daniel Everland <danieleverland@gmail.com>
2018-04-13 01:21:03 +0200

    Serialization init

---
+++ Unity-Mod-Serializer/Converters/BaseConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UMS.Converters
+{
+    /// <summary>
+    /// Converters converts object from memory into a serializable data format.
+    /// </summary>
+    public abstract class BaseConverter<T>
+    {
+        /// <summary>
+        /// The type this converter supports
+        /// </summary>
+        public System.Type ModelType { get { return typeof(T); } }
+        
+        public abstract Result Serialize(T value, out Data data);
+        public abstract Result Deserialize(Data data, out T obj);
+    }
+}
---
+++ Unity-Mod-Serializer/Serializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UMS
+{
+    public static class Serializer
+    {
+        public static Result Serialize(object value, out Data data)
+        {
+            Result result = Result.Success;
+
+            data = Data.Null;
+
+            return result;
+        }
+    }
+}
