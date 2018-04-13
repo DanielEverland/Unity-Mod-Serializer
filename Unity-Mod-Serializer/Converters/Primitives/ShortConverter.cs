ec6b5e97dc6723a76fc9ef1f71d4bf17eda6188d
Daniel Everland <danieleverland@gmail.com>
2018-04-13 17:01:54 +0200

    Added short converter

---
+++ Unity-Mod-Serializer/Converters/Primitives/ShortConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UMS.Converters.Primitives
+{
+    public class ShortConverter : DirectConverter<short>
+    {
+        public override Result Serialize(short obj, out Data data)
+        {
+            data = new Data(obj);
+            return Result.Success;
+        }
+        public override Result Deserialize(Data data, ref short obj)
+        {
+            if (!data.IsLong)
+                return Result.Error("Type mismatch. Expected Long", data);
+
+            obj = System.Convert.ToInt16(data.AsLong);
+            return Result.Success;
+        }
+    }
+}
