007b5929b52bffe1b226d007da72f44bfeef7bae
Daniel Everland <danieleverland@gmail.com>
2018-04-13 16:55:01 +0200

    Added long converter

---
+++ Unity-Mod-Serializer/Converters/Primitives/LongConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UMS.Converters.Primitives
+{
+    public class LongConverter : DirectConverter<long>
+    {
+        public override Result Serialize(long obj, out Data data)
+        {
+            data = new Data(obj);
+            return Result.Success;
+        }
+        public override Result Deserialize(Data data, ref long obj)
+        {
+            if (!data.IsLong)
+                return Result.Error("Type mismatch. Expected Long", data);
+
+            obj = data.AsLong;
+            return Result.Success;
+        }
+    }
+}
