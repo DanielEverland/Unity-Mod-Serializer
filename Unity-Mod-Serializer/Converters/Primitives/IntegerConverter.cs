f1a31df6b0a9f63a51e6255a691f1179ea81399f
Daniel Everland <danieleverland@gmail.com>
2018-04-13 16:34:41 +0200

    Added int converter

---
+++ Unity-Mod-Serializer/Converters/Primitives/IntegerConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UMS.Converters.Primitives
+{
+    public class IntegerConverter : DirectConverter<int>
+    {
+        public override Result Serialize(int obj, out Data data)
+        {
+            data = new Data(obj);
+            return Result.Success;
+        }
+        public override Result Deserialize(Data data, ref int obj)
+        {
+            if (!data.IsLong)
+                return Result.Error("Type mismatch. Expected Long", data);
+
+            obj = System.Convert.ToInt32(data.AsLong);
+            return Result.Success;
+        }
+    }
+}
