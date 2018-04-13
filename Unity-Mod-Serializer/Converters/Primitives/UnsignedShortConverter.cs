021bdbd6da34d21e4db5c1d4021eeee36b3cd01e
Daniel Everland <danieleverland@gmail.com>
2018-04-13 17:03:55 +0200

    Added ushort converter

---
+++ Unity-Mod-Serializer/Converters/Primitives/UnsignedShortConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UMS.Converters.Primitives
+{
+    public class UnsignedShortConverter : DirectConverter<ushort>
+    {
+        public override Result Serialize(ushort obj, out Data data)
+        {
+            data = new Data(obj);
+            return Result.Success;
+        }
+        public override Result Deserialize(Data data, ref ushort obj)
+        {
+            if (!data.IsLong)
+                return Result.Error("Type mismatch. Expected Long", data);
+
+            obj = System.Convert.ToUInt16(data.AsLong);
+            return Result.Success;
+        }
+    }
+}
