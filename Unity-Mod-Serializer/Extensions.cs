9f8186710f4c2d55376eb78588acbf8b191a638c
Daniel Everland <danieleverland@gmail.com>
2018-04-13 17:21:22 +0200

    Implemented protobuf serialization

---
+++ Unity-Mod-Serializer/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ProtoBuf;
+
+namespace UMS
+{
+    public static class Extensions
+    {
+        public static T Deserialize<T>(this byte[] array)
+        {
+            using (MemoryStream stream = new MemoryStream(array))
+            {
+                return ProtoBuf.Serializer.Deserialize<T>(stream);
+            }
+        }
+        public static byte[] SerializeToBytes(this Data data)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                ProtoBuf.Serializer.Serialize(stream, data);
+                return stream.ToArray();
+            }
+        }
+    }
+}
--- Unity-Mod-Serializer/Serializer.cs
+++ Unity-Mod-Serializer/Serializer.cs
@@ -17,6 +17,79 @@ namespace UMS
         private static Dictionary<System.Type, IDirectConverter> _directConverters;
         private static List<IBaseConverter> _converters;
 
+        /// <summary>
+        /// Deserializes a byte array into memory
+        /// </summary>
+        public static Result Deserialize<T>(byte[] array, ref T obj)
+        {
+            Result result = Result.Success;
+
+            System.Type objType = typeof(T);
+            IBaseConverter converter = GetConverter(objType);
+
+            object boxedObject = null;
+            result += converter.Deserialize(array.Deserialize<Data>(), ref boxedObject);
+
+            obj = (T)boxedObject;
+
+            return result;
+        }
+        /// <summary>
+        /// Deserializes a byte array into memory
+        /// </summary>
+        public static Result Deserialize(byte[] array, System.Type objType, ref object obj)
+        {
+            Result result = Result.Success;
+
+            Data data = array.Deserialize<Data>();
+            Deserialize(data, objType, ref obj);
+
+            return result;
+        }
+        /// <summary>
+        /// Deserializes a Data object into memory
+        /// </summary>
+        public static Result Deserialize<T>(Data data, ref T obj)
+        {
+            Result result = Result.Success;
+
+            System.Type objType = typeof(T);
+            IBaseConverter converter = GetConverter(objType);
+
+            object boxedObject = null;
+            result += converter.Deserialize(data, ref boxedObject);
+
+            obj = (T)boxedObject;
+
+            return result;
+        }
+        /// <summary>
+        /// Deserializes a Data object into memory
+        /// </summary>
+        public static Result Deserialize(Data data, System.Type objType, ref object obj)
+        {
+            Result result = Result.Success;
+            
+            IBaseConverter converter = GetConverter(objType);
+            result += converter.Deserialize(data, ref obj);
+
+            return result;
+        }
+        /// <summary>
+        /// Serializes an object into a byte array
+        /// </summary>
+        public static Result Serialize(object value, out byte[] array)
+        {
+            Result result = Result.Success;
+
+            result += Serialize(value, out Data data);
+            array = data.SerializeToBytes();
+
+            return result;
+        }
+        /// <summary>
+        /// Serializes an object into a Data object
+        /// </summary>
         public static Result Serialize(object value, out Data data)
         {
             Result result = Result.Success;
