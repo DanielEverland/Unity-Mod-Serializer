e996b9b13374af1e85ee4bf8ce72c84afdd29dc1
Daniel Everland <danieleverland@gmail.com>
2018-04-13 16:16:48 +0200

    Added byteconverter

---
+++ Unity-Mod-Serializer/Converters/Primitives/ByteConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UMS.Converters.Primitives
+{
+    public class ByteConverter : DirectConverter<byte>
+    {
+        public override Result Serialize(byte obj, out Data data)
+        {
+            data = new Data(obj);
+            return Result.Success;
+        }
+        public override Result Deserialize(Data data, ref byte obj)
+        {
+            if (!data.IsLong)
+                return Result.Error("Type mismatch. Expected long type", data);
+
+            obj = (byte)System.Convert.ChangeType(data.AsLong, typeof(byte));
+            return Result.Success;
+        }
+    }
+}
