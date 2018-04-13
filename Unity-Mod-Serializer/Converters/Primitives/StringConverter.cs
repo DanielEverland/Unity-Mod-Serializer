d81daa91bcd4dc231d8852511c276e7445092fa9
Daniel Everland <danieleverland@gmail.com>
2018-04-13 17:05:52 +0200

    Added string converter

---
+++ Unity-Mod-Serializer/Converters/Primitives/StringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UMS.Converters.Primitives
+{
+    public class StringConverter : DirectConverter<string>
+    {
+        public override Result Serialize(string obj, out Data data)
+        {
+            data = new Data(obj);
+            return Result.Success;
+        }
+        public override Result Deserialize(Data data, ref string obj)
+        {
+            if (!data.IsString)
+                return Result.Error("Type mismatch. Expected String", data);
+
+            obj = data.AsString;
+            return Result.Success;
+        }
+    }
+}
