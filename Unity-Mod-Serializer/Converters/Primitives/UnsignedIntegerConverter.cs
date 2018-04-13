8a82aeaf267f4993380211dfd8b31813df48876e
Daniel Everland <danieleverland@gmail.com>
2018-04-13 16:52:05 +0200

    Added uint converter

---
+++ Unity-Mod-Serializer/Converters/Primitives/UnsignedIntegerConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UMS.Converters.Primitives
+{
+    public class UnsignedIntegerConverter : DirectConverter<uint>
+    {
+        public override Result Serialize(uint obj, out Data data)
+        {
+            data = new Data(obj);
+            return Result.Success;
+        }
+        public override Result Deserialize(Data data, ref uint obj)
+        {
+            if (!data.IsLong)
+                return Result.Error("Type mismatch. Expected Long", data);
+
+            obj = System.Convert.ToUInt32(data.AsLong);
+            return Result.Success;
+        }
+    }
+}
