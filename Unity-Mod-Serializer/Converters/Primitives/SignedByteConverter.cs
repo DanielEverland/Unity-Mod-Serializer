699a16c6f4206e442bfbddbc68f461340ae32a33
Daniel Everland <danieleverland@gmail.com>
2018-04-13 16:21:20 +0200

    Added signed byte converter

---
+++ Unity-Mod-Serializer/Converters/Primitives/SignedByteConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UMS.Converters.Primitives
+{
+    public class SignedByteConverter : DirectConverter<sbyte>
+    {
+        public override Result Serialize(sbyte obj, out Data data)
+        {
+            data = new Data(obj);
+            return Result.Success;
+        }
+        public override Result Deserialize(Data data, ref sbyte obj)
+        {
+            if (!data.IsLong)
+                return Result.Error("Type mismatch. Expected Long", data);
+
+            obj = (sbyte)System.Convert.ChangeType(data.AsLong, typeof(sbyte));
+            return Result.Success;
+        }
+    }
+}
