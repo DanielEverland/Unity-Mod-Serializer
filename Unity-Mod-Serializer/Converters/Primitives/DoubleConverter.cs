10660fcd1ff86451ce45354a7c15307fd1881983
Daniel Everland <danieleverland@gmail.com>
2018-04-13 16:29:34 +0200

    Added double converter

---
+++ Unity-Mod-Serializer/Converters/Primitives/DoubleConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UMS.Converters.Primitives
+{
+    public class DoubleConverter : DirectConverter<double>
+    {
+        public override Result Serialize(double obj, out Data data)
+        {
+            data = new Data(obj);
+            return Result.Success;
+        }
+        public override Result Deserialize(Data data, ref double obj)
+        {
+            if (!data.IsDouble)
+                return Result.Error("Type mismatch. Expected Double", data);
+
+            obj = data.AsDouble;
+            return Result.Success;
+        }
+    }
+}
