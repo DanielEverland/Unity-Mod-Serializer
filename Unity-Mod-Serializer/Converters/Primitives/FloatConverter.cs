6e2f98fb04ec180e191421632c0b6a76493ddf20
Daniel Everland <danieleverland@gmail.com>
2018-04-13 16:31:51 +0200

    Added float converter

---
+++ Unity-Mod-Serializer/Converters/Primitives/FloatConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UMS.Converters.Primitives
+{
+    public class FloatConverter : DirectConverter<float>
+    {
+        public override Result Serialize(float obj, out Data data)
+        {
+            data = new Data(obj);
+            return Result.Success;
+        }
+        public override Result Deserialize(Data data, ref float obj)
+        {
+            if (!data.IsDouble)
+                return Result.Error("Type mismatch. Expected Double", data);
+
+            obj = System.Convert.ToSingle(data.AsDouble);
+            return Result.Success;
+        }
+    }
+}
