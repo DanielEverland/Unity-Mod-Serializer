96e31eba3e1d06153a8e0e036111b610b4fd8788
Daniel Everland <danieleverland@gmail.com>
2018-05-14 16:44:46 +0200

    Benchmarking update

--- Unity-Mod-Serializer-Editor/Editor/StressTest.cs
+++ Unity-Mod-Serializer-Editor/Editor/StressTest.cs
@@ -15,15 +15,37 @@ namespace UMS.Editor
     /// </summary>
     public static class StressTest
     {
-        private static TestData toSerialize;
-
         [MenuItem("Modding/Stress Test", priority = 1000)]
         public static void RunTest()
         {
-            
+            TestData data = GetNewData();
+
+            long byteLength = 0;
+            Benchmarker.Profile("Serialization", ITERATIONS, () =>
+            {
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    ProtoBuf.Serializer.Serialize(stream, data);
+                    byteLength += stream.ToArray().Length;
+                }                
+            });
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                ProtoBuf.Serializer.Serialize(stream, data);
+
+                byte[] dataToDeserialize = stream.ToArray();
+
+                Benchmarker.Profile("Deserialization", ITERATIONS, () =>
+                {
+                    ProtoBuf.Serializer.Deserialize<TestData>(new MemoryStream(dataToDeserialize));
+                });
+            }
+
+            UnityEngine.Debug.Log($"Serialized size is in total: {byteLength.ToString("#,##")} bytes, average: {((long)(double)byteLength / ITERATIONS).ToString("#,##")} bytes");
         }
 
-        private const int ITERATIONS = 1000;
+        private const int ITERATIONS = 10000;
         
         [ProtoContract]
         public class TestData
---
+++ Unity-Mod-Serializer/Benchmarker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace UMS
+{
+    public static class Benchmarker
+    {
+        public static double Profile(string name, int iterations, Action func)
+        {
+            //Run at highest priority to minimize fluctuations caused by other processes/threads
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
+            Thread.CurrentThread.Priority = ThreadPriority.Highest;
+
+            // warm up 
+            func();
+
+            var watch = new Stopwatch();
+
+            // clean up
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            watch.Start();
+            for (int i = 0; i < iterations; i++)
+            {
+                func();
+            }
+            watch.Stop();
+
+            double total = watch.Elapsed.TotalMilliseconds;
+
+            UnityEngine.Debug.Log($"{name} ran {iterations.ToString("#,##")} times, elapsed total: {total.ToString("F0")}ms, average: {total / iterations}ms");
+
+            return total;
+        }
+    }
+}
