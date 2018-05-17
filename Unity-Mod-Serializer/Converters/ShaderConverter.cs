073bf728a92713483e4d96284f9ff2888abcd318
Daniel Everland <danieleverland@gmail.com>
2018-05-17 18:53:09 +0200

    Implemented shader converter

---
+++ Unity-Mod-Serializer/Converters/ShaderConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace UMS.Converters
+{
+    public class ShaderConverter : DirectConverter<Shader>
+    {
+        public override Result DoSerialize(Shader obj, out Data data)
+        {
+            data = new Data(obj.name);
+
+            return Result.Success;
+        }
+        public override Result DoDeserialize(Data data, ref Shader obj)
+        {
+            if (!data.IsString)
+                return Result.Error("Type mismatch. Expected string");
+
+            obj = Shader.Find(data.String);
+
+            return Result.Success;
+        }        
+    }
+}
