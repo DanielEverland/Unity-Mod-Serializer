d7ed7dd960f5786124e1df9595ff68fb073cd1a2
Daniel Everland <danieleverland@gmail.com>
2018-05-21 00:14:04 +0200

    Added byte wrapper

---
+++ Unity-Mod-Serializer/Wrappers/ByteWrapper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ProtoBuf;
+
+namespace UMS.Wrappers
+{
+    [ProtoContract]
+    public class ByteWrapper
+    {
+        [ProtoMember(1)]
+        private byte _value;
+
+        public static ByteWrapper Create(byte value)
+        {
+            return value;
+        }
+        public static implicit operator ByteWrapper(byte value)
+        {
+            return new ByteWrapper() { _value = value };
+        }
+        public static implicit operator byte(ByteWrapper wrapper)
+        {
+            return wrapper._value;
+        }
+    }
+}
