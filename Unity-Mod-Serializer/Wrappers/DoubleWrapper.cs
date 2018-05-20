f313bd107f3d12b4e248404566e08138ffe96930
Daniel Everland <danieleverland@gmail.com>
2018-05-21 00:19:45 +0200

    Added double wrapper

---
+++ Unity-Mod-Serializer/Wrappers/DoubleWrapper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ProtoBuf;
+
+namespace UMS.Wrappers
+{
+    [ProtoContract]
+    public class DoubleWrapper
+    {
+        [ProtoMember(1)]
+        private double _value;
+
+        public static DoubleWrapper Create(double value)
+        {
+            return value;
+        }
+        public static implicit operator DoubleWrapper(double value)
+        {
+            return new DoubleWrapper() { _value = value };
+        }
+        public static implicit operator double(DoubleWrapper wrapper)
+        {
+            return wrapper._value;
+        }
+    }
+}
