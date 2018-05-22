85b9cce44c177c384e17a50e40193c9f38b6c71b
Daniel Everland <danieleverland@gmail.com>
2018-05-22 22:55:38 +0200

    Component init

--- Unity-Mod-Serializer/Operators/GameObjectOperator.cs
+++ Unity-Mod-Serializer/Operators/GameObjectOperator.cs
@@ -20,16 +20,29 @@ namespace UMS.Operators
             public GameObjectModel(GameObject obj)
             {
                 name = obj.name;
+
+                components = new List<SerializableComponent>();
+                foreach (Component comp in obj.GetComponents<Component>())
+                {
+                    components.Add(new SerializableComponent(comp));
+                }
             }
 
             [ProtoMember(1)]
             private string name;
+            [ProtoMember(2)]
+            private List<SerializableComponent> components;
 
             [ProtoAfterDeserialization]
-            private void BeforeDeserialize()
+            private void Deserialize()
             {
                 GameObject obj = new GameObject();
                 obj.name = name;
+
+                foreach (SerializableComponent comp in components)
+                {
+                    comp.Deserialize(obj);
+                }
             }
         }
     }
---
+++ Unity-Mod-Serializer/SerializableComponent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using ProtoBuf;
+
+namespace UMS
+{
+    [ProtoContract]
+    public class SerializableComponent
+    {
+        public SerializableComponent() { }
+        public SerializableComponent(Component component)
+        {
+            type = component.GetType();
+        }
+
+        [ProtoMember(1)]
+        private System.Type type;
+        
+        public void Deserialize(GameObject obj)
+        {
+            Component component = GetComponent(obj);
+        }
+        private Component GetComponent(GameObject obj)
+        {
+            if (typeof(Transform).IsAssignableFrom(type))
+                return obj.GetComponent(type);
+
+            return obj.AddComponent(type);
+        }
+    }
+}
