3d4152e363c8db96c2a417bb9bf666d5b40ed7a8
Daniel Everland <danieleverland@gmail.com>
2018-05-24 01:37:41 +0200

    Added comment explaining why data class is necessary

--- Unity-Mod-Serializer/Data.cs
+++ Unity-Mod-Serializer/Data.cs
@@ -6,6 +6,16 @@ using ProtoBuf;
 
 namespace UMS
 {
+    // We have to wrap out proto data in this class, otherwise we encounter a myriad of exceptions
+    // A better alternative would be to serialize component data as a Dictionary[int, object],
+    // with the DynamicType attribute property set to true. Unfortunately that results in two issues
+
+    // The first is that the object *must* be a contract, so we can't serialize primitives directly.
+    // This is easily "fixable" by creating a contract for each type, although that's definitely a hack.
+
+    // The second issue is that we get an "Unknown sub-type" exception. That has me stumped. This is,
+    // obviously, usually because we haven't used the [ProtoInclude] attribute on inherited types. Thing
+    // is, though, that none of the types have *any* inhertiance at all. Perhaps a bug in protobuf?
     [ProtoContract]
     public class Data
     {
