28d84fa87fdd3331c344e7d2939164358da8b390
Daniel Everland <danieleverland@gmail.com>
2018-05-24 01:32:27 +0200

    Added data class

---
+++ Unity-Mod-Serializer/Data.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using ProtoBuf;
+
+namespace UMS
+{
+    [ProtoContract]
+    public class Data
+    {
+        [ProtoMember(1)]
+        private int memberID;
+        [ProtoMember(2)]
+        private System.Type type;
+        [ProtoMember(3)]
+        private byte[] data;
+    }
+}
