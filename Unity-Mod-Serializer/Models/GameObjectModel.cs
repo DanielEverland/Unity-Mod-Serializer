cf007194bfb0aa0a935ad93e8a5619d601ce0bb0
Daniel Everland <danieleverland@gmail.com>
2018-05-24 02:10:38 +0200

    Init GameObject model

---
+++ Unity-Mod-Serializer/Models/GameObjectModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using ProtoBuf;
+using ProtoBuf.Meta;
+
+namespace UMS.Models
+{
+    public class GameObjectModel : ModelBase<GameObject>
+    {
+        public override void CreateModel(MetaType type)
+        {
+            type.AsReferenceDefault = true;
+            type.SetSurrogate(typeof(GameObjectSurrogate));
+        }
+
+        [ProtoContract]
+        private class GameObjectSurrogate
+        {
+            public GameObjectSurrogate() { }
+            public GameObjectSurrogate(GameObject obj)
+            {
+                name = obj.name;
+            }
+
+            [ProtoMember(1)]
+            private string name;
+
+            public GameObject Deserialize()
+            {
+                GameObject obj = new GameObject();
+
+                obj.name = name;
+
+                return obj;
+            }
+
+            public static implicit operator GameObjectSurrogate (GameObject obj)
+            {
+                return obj == null ? null : new GameObjectSurrogate(obj);
+            }
+            public static implicit operator GameObject (GameObjectSurrogate surrogate)
+            {
+                return surrogate == null ? null : surrogate.Deserialize();
+            }
+        }
+    }
+}
