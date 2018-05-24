623b9ce0ecf1ae284ab8cca81b6177d6b42befc8
Daniel Everland <danieleverland@gmail.com>
2018-05-24 07:19:07 +0200

    Added boneweight model

---
+++ Unity-Mod-Serializer/Models/BoneWeightModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ProtoBuf.Meta;
+using UnityEngine;
+
+namespace UMS.Models
+{
+    public class BoneWeightModel : ModelBase<BoneWeight>
+    {
+        public override void CreateModel(MetaType type)
+        {
+            type.Add("weight0", "weight1", "weight2", "weight3");
+            type.Add("boneIndex0", "boneIndex1", "boneIndex2", "boneIndex3");
+        }
+    }
+}
