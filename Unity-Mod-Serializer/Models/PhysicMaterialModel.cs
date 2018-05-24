5914025835e443e2687fa68f859ba3bb0a97290a
Daniel Everland <danieleverland@gmail.com>
2018-05-24 07:28:43 +0200

    Added PhysicMaterial model

---
+++ Unity-Mod-Serializer/Models/PhysicMaterialModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ProtoBuf.Meta;
+using UnityEngine;
+
+namespace UMS.Models
+{
+    public class PhysicMaterialModel : ModelBase<PhysicMaterial>
+    {
+        public override void CreateModel(MetaType type)
+        {
+            type.AsReferenceDefault = true;
+            type.Add("name", "dynamicFriction", "staticFriction", "bounciness", "frictionCombine");
+        }
+    }
+}
