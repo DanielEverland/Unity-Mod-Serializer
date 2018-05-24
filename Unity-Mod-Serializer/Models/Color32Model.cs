f8cb0fe1f170250df410c0a57eafc69da87a71c3
Daniel Everland <danieleverland@gmail.com>
2018-05-24 07:25:50 +0200

    Added color models

---
+++ Unity-Mod-Serializer/Models/Color32Model.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ProtoBuf.Meta;
+using UnityEngine;
+
+namespace UMS.Models
+{
+    public class Color32Model : ModelBase<Color32>
+    {
+        public override void CreateModel(MetaType type)
+        {
+            type.Add("r", "g", "b", "a");
+        }
+    }
+}
---
+++ Unity-Mod-Serializer/Models/ColorModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using ProtoBuf.Meta;
+using UnityEngine;
+
+namespace UMS.Models
+{
+    public class ColorModel : ModelBase<Color>
+    {
+        public override void CreateModel(MetaType type)
+        {
+            type.SetSurrogate(typeof(Color32));
+        }
+    }
+}
